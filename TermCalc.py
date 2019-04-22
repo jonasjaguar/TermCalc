ea4a7e819dad36d666aeebfb0cb9fbb35b873cf8
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2019-04-22 19:50:03 +0200

    do the repeat thing

--- TermCalc.py
+++ TermCalc.py
@@ -1,23 +1,27 @@
 print("Welcome to TermCalc, the Terminal-Based Calculator.")
+continue = "Y"
 
+while continue == "Y" or continue == "y":
+    ## Input Types ##
+    n1 = float(input("First Number: "))
+    op = input("Which Operator?(+, -, *, /, %): ")
+    n2 = float(input("Second Number: "))
 
-## Input Types ##
-n1 = float(input("First Number: "))
-op = input("Which Operator?(+, -, *, /, %): ")
-n2 = float(input("Second Number: "))
+    ## Define what to do ##
 
-## Define what to do ##
+    if op == "+":
+        print(n1 + n2)
+    elif op == "-":
+        print(n1 - n2)
+    elif op == "/":
+        print(n1 / n2)
+    elif op == "*":
+        print(n1 * n2)
+    elif op == "%":
+        print((n1/n2 * 100.0))
+    else:
+        print("Invalid Input, please type a right operator!")
+    
+    continue = input("Another calculation? (Y/N) ")
 
-if op == "+":
-    print(n1 + n2)
-elif op == "-":
-    print(n1 - n2)
-elif op == "/":
-    print(n1 / n2)
-elif op == "*":
-    print(n1 * n2)
-elif op == "%":
-    print((n1/n2 * 100.0))
-
-else:
-    print("Invalid Input, please type a right operator!")
+print("Goodbye!")
